d5172ff7bdcb8f0e13f09e5a628029f3e3638df2
galatea21 <vietthidinh2001@gmail.com>
2021-04-10 01:44:02 +0300

    Add register/sign_in interface

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:resonote/domain/auth/auth_failure.dart';
+import 'package:resonote/domain/auth/value_object.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    required EmailAddress email,
+    required Password password,
+  });
+
+	Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    required EmailAddress email,
+    required Password password,
+  });
+
+	Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
