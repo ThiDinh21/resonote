0bb545578fb08eb0cb32081532cd15e99f29e632
galatea21 <vietthidinh2001@gmail.com>
2021-04-10 01:10:50 +0300

    Add email, password objects, validators

---
+++ lib/domain/core/value_object.dart
@@ -0,0 +1,24 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+import 'package:dartz/dartz.dart';
+
+import 'package:resonote/domain/core/failures.dart';
+
+@immutable
+abstract class ValueObject<T> {
+  const ValueObject();
+
+  Either<ValueFailure<T>, T> get value;
+
+  @override
+  String toString() => 'Value($value)';
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+    return o is ValueObject && o.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+}
