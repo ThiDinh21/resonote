451c35d20539365206087f792068b98be2d4e069
galatea21 <vietthidinh2001@gmail.com>
2021-04-10 19:05:27 +0300

    Create Bloc for register + sign in logic

---
+++ lib/application/auth/sign_in_form/bloc/sign_in_form_bloc.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:meta/meta.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:resonote/domain/auth/auth_failure.dart';
+import 'package:resonote/domain/auth/i_auth_facade.dart';
+import 'package:resonote/domain/auth/value_object.dart';
+
+part 'sign_in_form_event.dart';
+part 'sign_in_form_state.dart';
+part 'sign_in_form_bloc.freezed.dart';
+
+class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  final IAuthFacade _authFacade;
+
+  SignInFormBloc(this._authFacade) : super(SignInFormState.initial());
+
+  @override
+  Stream<SignInFormState> mapEventToState(
+    SignInFormEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
